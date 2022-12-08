ddb4c7e04f367a564e4d52446a6dbb0834358490
christylicy <120062854+christylicy@users.noreply.github.com>
2022-12-07 20:46:47 -0500

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -9,36 +9,9 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
-
-  // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
-
-  // default notion icon and cover images for site-wide consistency (optional)
-  // page-specific values will override these site-wide defaults
-  defaultPageIcon: null,
-  defaultPageCover: null,
-  defaultPageCoverPosition: 0.5,
-
-  // whether or not to enable support for LQIP preview images (optional)
-  isPreviewImageSupportEnabled: true,
-
-  // whether or not redis is enabled for caching generated preview images (optional)
-  // NOTE: if you enable redis, you need to set the `REDIS_HOST` and `REDIS_PASSWORD`
-  // environment variables. see the readme for more info
-  isRedisEnabled: false,
-
-  // map of notion page IDs to URL paths (optional)
-  // any pages defined here will override their default URL paths
-  // example:
-  //
-  // pageUrlOverrides: {
-  //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
-  //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
-  // }
-  pageUrlOverrides: null,
+  name: 'christyischunyanli',
+  domain: 'christyischunyanli.vercel.app',
+  author: 'Chunyan li',
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
